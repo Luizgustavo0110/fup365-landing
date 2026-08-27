d0467a246e16ddcd4cd56984fcd03f6b17333a69
Luiz Gustavo Miranda <luizgpmiranda@gmail.com>
2026-08-27 10:17:56 -0300

    chore: update contact links and hero spacing

--- src/config/links.ts
+++ src/config/links.ts
@@ -8,7 +8,7 @@ export const externalLinks = {
     'https://bookings.cloud.microsoft/book/DemonstraesFUP365@telog.com.br/s/WncH-Rsb40GkgC2F8fvDYw2?ismsaljsauthenabled',
 
   whatsapp:
-    'https://wa.me/5516997400361?text=Ol%C3%A1!%20Vim%20pelo%20site%20do%20FUP365%20e%20gostaria%20de%20saber%20mais%20sobre%20a%20solu%C3%A7%C3%A3o',
+    'https://wa.me/5516997574050?text=Ol%C3%A1!%20Vim%20pelo%20site%20do%20FUP365%20e%20gostaria%20de%20saber%20mais%20sobre%20a%20solu%C3%A7%C3%A3o',
 
-  email: 'mailto:luiz.miranda@linklog.com.br?subject=Contato%20pelo%20site%20FUP365',
+  email: 'mailto:contato@linklog.com.br?subject=Contato%20pelo%20site%20FUP365',
 } as const;
